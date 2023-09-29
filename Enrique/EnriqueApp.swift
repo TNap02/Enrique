15472955697a5a68815609e5ff3aa0a91bddcc43
TNap02 <napier.tristan@gmail.com>
2023-09-29 15:13:15 -0400

    Initial Commit

---
+++ Enrique/EnriqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnriqueApp.swift
+//  Enrique
+//
+//  Created by Tristan Napier on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct EnriqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
